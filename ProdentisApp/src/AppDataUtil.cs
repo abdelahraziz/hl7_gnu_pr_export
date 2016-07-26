5479a895f3d30816b38afa8ee99b789a6f124b60
Abe Lenstra <abelenstra@gmail.com>
2016-07-26 16:53:18 +0200

    import project

---
+++ ProdentisApp/src/AppDataUtil.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProdentisApp
+{
+    class AppDataUtil
+    {
+        private static StringArray box(string str)
+        {
+            StringArray array = new StringArray();
+            array.Insert(str);
+            return array;
+        }
+
+        public static Patients processProdentis(DBUtil.DBResult result)
+        {
+            Patients Patients = new Patients();
+            for (int i = 0; i < result.Count; i++)
+            {
+                List<string> row = result[i];
+                Patient Patient = HL7Util.getEmptyPatient();
+                HL7Util.addIdentifier(ref Patient, "Card", row[0]);
+                HL7Util.setFirstName(ref Patient, row[1]);
+                HL7Util.setSecondName(ref Patient, row[2]);
+                HL7Util.setFamily(ref Patient, row[3]);
+                if (row[8].Contains("1"))
+                    HL7Util.setGender(ref Patient, "male"); 
+                else
+                    HL7Util.setGender(ref Patient, "female"); 
+                HL7Util.addStreetPart(ref Patient, row[5]);
+                HL7Util.addStreetPart(ref Patient, row[6]);
+                HL7Util.addStreetPart(ref Patient, row[7]);
+                HL7Util.setPostalCode(ref Patient, row[8]);
+                HL7Util.setCity(ref Patient, row[9]);
+                HL7Util.addIdentifier(ref Patient, "gmina", row[10]);
+                HL7Util.addIdentifier(ref Patient, "miasto", row[11]);
+                HL7Util.addIdentifier(ref Patient, "wojewodztwo", row[12]);
+                HL7Util.setCountry(ref Patient, row[13]);
+                HL7Util.setVatCode(ref Patient, row[14]);
+                HL7Util.setPatientalCode(ref Patient, row[15]);
+                HL7Util.setBirthDate(ref Patient, row[16].Replace("00:00:00",""));
+                HL7Util.addIdentifier(ref Patient, "birthPlace", row[17]);
+                HL7Util.addContact(ref Patient, "email", row[18]);
+                HL7Util.addContact(ref Patient, "phone", row[19]);
+                HL7Util.addContact(ref Patient, "phone", row[20]);
+                HL7Util.addContact(ref Patient, "phone", row[21]);
+                HL7Util.addIdentifier(ref Patient, "smsReceiver", row[22]);
+                HL7Util.addIdentifier(ref Patient, "emailReceiver", row[23]);
+                HL7Util.addIdentifier(ref Patient, "IdentityDocumentNumber", row[24]);
+                HL7Util.addIdentifier(ref Patient, "IdentityDocumentType", "Paszport");
+                HL7Util.addIdentifier(ref Patient, "CompanyNIP", row[25]);
+                HL7Util.addIdentifier(ref Patient, "NfzDepartmentCode", row[26].Replace("R",""));
+                Patients.Insert(Patient);
+            }
+            return Patients;
+        }
+    }
+}
---
+++ ProdentisApp/src/MainThread.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace ProdentisApp
+{
+    class MainThread : HL7Util.HL7Cb
+    {
+        private MainThreadCb ui;
+        private string host;
+        private int port;
+        private string dbname;
+        private string user;
+        private string password;
+        private int rowIndex;
+        private int rowCount;
+
+        public MainThread(MainThreadCb _ui, string _host, int _port, string _dbname, string _user, string _password)
+        {
+            ui = _ui;
+            host = _host;
+            port = _port;
+            dbname = _dbname;
+            user = _user;
+            password = _password;
+        }
+
+        public interface MainThreadCb : DBUtil.ConnectCb, HL7Util.HL7Cb, NetUtil.ScanPortsCb
+        {
+            void onError(string msg);
+        }
+
+        public void processProdentis()
+        {
+            try
+            {
+                DBUtil.MSSqlDBConnection db = DBUtil.MSSqlDBConnection.Instance();
+                if (db.connect(ui, host, port, dbname, user, password))
+                {
+                    int loop = 1;
+                    string tmpFile = System.IO.Path.GetTempPath().ToString() + DateTime.Now.ToString("yyyyMMddHHmmssfff") + "hl7dbexport.hl7";
+                    DBUtil.DBResult countResult = db.query("Select count(*) from Prodentis500.dbo.pacjenci");
+                    rowCount = (Int32.Parse(countResult[0][0]) / 1000) + 1;
+                    while (true)
+                    {
+                        string q = "Select nr_kartywew, imie, imie2, nazwisko, plec, ulica, num_domu, num_mieszkania, kod_pocztowy, miasto, g.nazwa, kod_miasta, w.wojewodztwo, c.nazwa, nip, pesel, data_urodzenia, miejsce_ur, email, telefonypraca, telefonydom, komorka, wys_sms, wys_email, paszport, nip, symbol_kch from Prodentis500.dbo.pacjenci p left join Prodentis500.dbo.s_oddzialy_NFZ nfzo on p.symbol_kch = nfzo.symbol_k left join Prodentis500.dbo.s_kraje c on c.id_kraju = p.id_kraju left join Prodentis500.dbo.s_wojewodztwa w on  p.kod_wojew = w.kod left join Prodentis500.dbo.s_gminy g on p.kod_gminy = g.kod order by pesel offset " + ((loop - 1) * 1000).ToString() + " rows FETCH NEXT " + (loop * 1000).ToString() + " ROWS ONLY";
+                        DBUtil.DBResult result = db.query(q);
+                        Patients patients = AppDataUtil.processProdentis(result);
+                        if (patients.Size() == 0)
+                        {
+                            ui.onHL7Done(tmpFile);
+                            db.Close();
+                            return;
+                        }
+                        string stream = "";
+                        HL7Util.processPatients(this, ref stream, patients, tmpFile);
+                        loop++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void onHL7Progress(double progress)
+        {
+            double res = progress;
+            res = 100 * ((res / 100) + rowIndex) / rowCount;
+            ui.onHL7Progress(res);
+        }
+
+        public void onHL7Done(string tmpFile)
+        {
+            rowIndex++;
+        }
+    }
+}
